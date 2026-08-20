ff774316bc4a2d5cd8a7d0fd5df24b659e2fda1e
Rohit Saini <rohitsaini0444@gmail.com>
2026-08-20 15:29:23 +0530

    Feature signup user

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -55,7 +55,7 @@ const EditProfile = ({ user }) => {
                 <div className="hero-content flex-col lg:flex-row-reverse p-4">
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold">Profile</h1>
-                        <UserCard user={{ firstName, lastName, age, gender, photoURL, about, skills }} />
+                        <UserCard user={{ firstName, lastName, age, gender, photoURL, about, skills, showButtons: false }} />
                     </div>
 
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,13 +8,15 @@ import { BASE_URL } from '../utils/constants';
 const Login = () => {
   const [email, setEmail] = useState('rohit.sharma@gmail.com')
   const [password, setPassword] = useState('Rohit@123')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
   const [error, setError] = useState(null)
+  const [isLoginForm, setIsLoginForm] = useState(true)
   const dispatch = useDispatch();
   const navigate = useNavigate();
- 
+
   const handleLogin = async (e) => {
     e.preventDefault()
-
     try {
       const response = await axios.post(`${BASE_URL}/auth/login`, { email, password }, { withCredentials: true })
       console.log('Login successful:', response.data);
@@ -23,19 +25,75 @@ const Login = () => {
       navigate('/feed');
     } catch (error) {
       setError(error.response?.data?.message || 'Login failed. Please try again.');
-      if(error.response && error.response.status === 401) {
-      console.error('Login failed:', error)
-    }
+      if (error.response && error.response.status === 401) {
+        console.error('Login failed:', error)
+      }
     }
 
+  }
+
+  const handleRegister = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post(`${BASE_URL}/auth/signup`, { firstName, lastName, email, password }, { withCredentials: true })
+      console.log('Registration successful:', response.data);
+      const user = response?.data?.user;
+      dispatch(setUser(user));
+      navigate('/profile');
+    } catch (error) {
+      setError(error.response?.data?.message || 'Registration failed. Please try again.');
+      if (error.response && error.response.status === 400) {
+        console.error('Registration failed:', error)
+      }
+    }
   }
 
   return (
     <div className="flex justify-center ">
       <div className="card card-border bg-base-100 w-96">
         <div className="card-body items-center text-center">
-          <h2 className="card-title">Login</h2>
+          <h2 className="card-title">{isLoginForm ? 'Login' : 'Sign Up'}</h2>
           <div className="card-actions justify-center">
+            {!isLoginForm && (<>
+              <label className="input validator">
+                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <g strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor">
+                    <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                    <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                  </g>
+                </svg>
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  required
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </label>
+              <label className="input validator">
+                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <g strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor">
+                    <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                    <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+                  </g>
+                </svg>
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  required
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </label>
+            </>)}
             <label className="input validator">
               <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <g
@@ -57,7 +115,8 @@ const Login = () => {
                 onChange={(e) => setEmail(e.target.value)}
               />
             </label>
-            <div className="validator-hint hidden">Enter valid email address</div>
+
+            <p className="validator-hint hidden">Enter valid email address</p>
             <label className="input validator">
               <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <g
@@ -89,8 +148,11 @@ const Login = () => {
               <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
             </p>
             <p className="text-red-500">{error}</p>
-            <button className="btn btn-primary" onClick={handleLogin}>
-              Login
+            <button className="btn btn-primary" onClick={isLoginForm ? handleLogin : handleRegister}>
+             {isLoginForm ? 'Login' : 'Sign Up'}
+            </button>
+            <button className="btn btn-link" onClick={() => setIsLoginForm(!isLoginForm)}>
+              {isLoginForm ? "Don't have an account? Sign Up" : 'Already have an account? Login'}
             </button>
           </div>
         </div>
